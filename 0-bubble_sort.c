794c1f4dd19a89e10a3ae19148f59a4d3df93c05
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-20 23:24:01 +0000

    Sorts using Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * swap - function to swap int
+ * @iz: first pointer to first position
+ * @xz: pointer to second position in array
+ * Return: void
+ */
+void swap(int *iz, int *xz)
+{
+	int temp = *iz;
+	*iz = *xz;
+	*xz = temp;
+}
+
+/**
+ * bubble_sort - sort using Bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, x;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (x = 0; x < size - i - 1; x++)
+		{
+			if (array[x] > array[x + 1])
+			{
+				swap(&array[x], &array[x + 1]);
+				print_array(array, size);
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+}
