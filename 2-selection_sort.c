87d3ab5084af45529b2900cb6965e77d70bb7ede
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-20 23:13:55 +0000

    fix file

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,19 +21,21 @@ void swap(int *iz, int *xz)
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx; 
-  
-	for (i = 0; i < size - 1; i++) 
-	{ 
-		min_idx = i; 
-		for (j = i + 1; j < size; j++) 
-		  if (array[j] < array[min_idx]) 
-			min_idx = j; 
-		
+	size_t	i;
+	size_t	j;
+	size_t	min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
 		if (min_idx != i)
 		{
 		swap(&array[min_idx], &array[i]);
 		print_array(array, size);
 		}
-	} 
+	}
 }
